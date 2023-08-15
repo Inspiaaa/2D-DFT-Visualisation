8d631b1941c3bf362e40424269f4f2ad74acc765
Inspiaaa <lavaafterburner@gmail.com>
2023-08-15 16:49:36 +0200

    Add visualisation of "visited" sinusoids

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from matplotlib.animation import FuncAnimation
 from PIL import Image
 import math
 
-
 IMAGE_SIZE = 256
 
 # Constant with which the sinusoid brightness is multiplied
@@ -14,6 +13,9 @@ BRIGHTNESS_FACTOR = 2
 # For values greater than 1 darker sinusoids appear brighter.
 BRIGHTNESS_BIAS = 2
 
+COLORMAP = plt.get_cmap("viridis")
+VISITED_COLORMAP = plt.get_cmap("plasma")
+
 
 def compute_2d_complex_sinusoid(size, freq_x, freq_y, coefficient):
     values = np.arange(size)
@@ -22,16 +24,28 @@ def compute_2d_complex_sinusoid(size, freq_x, freq_y, coefficient):
 
 
 class Animator:
-    def __init__(self, image_ax, layer_ax, fft):
+    def __init__(self, image_ax, layer_ax, fft_ax, fft):
         self.size = len(fft)
         self.image_ax = image_ax
         self.layer_ax = layer_ax
+        self.fft_ax = fft_ax
         self.fft = fft
 
         self.image = np.zeros((self.size, self.size), dtype=np.float)
+        # self.visited_fft_image = np.log10(np.abs(np.fft.fftshift(self.fft)) + 1)
+
+        self.normalized_fft_brightness = np.log10(np.abs(self.fft) + 1)
+        self.normalized_fft_brightness /= np.max(self.normalized_fft_brightness)
+
+        self.visited_fft_image = np.fft.fftshift(self.normalized_fft_brightness)
+        self.visited_fft_image /= np.max(self.visited_fft_image)
+        self.visited_fft_image = COLORMAP(self.visited_fft_image)[:, :, :3]
+
+        self.fft_ax.set_title("FFT")
+        self.fft_ax.imshow(self.visited_fft_image)
 
         frequency_count = math.ceil(self.size / 2)
-        self.frequencies_to_draw = [(x, y-127) for x in range(frequency_count) for y in range(self.size)]
+        self.frequencies_to_draw = [(x, y - 127) for x in range(frequency_count) for y in range(self.size)]
         self.frequencies_to_draw.sort(key=lambda pos: pos[0] ** 2 + pos[1] ** 2)
 
     def init(self):
@@ -60,6 +74,10 @@ class Animator:
 
         self.layer_ax.set_title(f"Sinusoid freq x={x} y={y}")
 
+        self.highlight_current_area(x, y)
+        self.highlight_current_area(-x, y)
+        self.fft_ax.imshow(self.visited_fft_image)
+
     def compute_value_range_for_brightness(self, x, y, coefficient):
         if x == y == 0:
             return 0, 255
@@ -74,6 +92,13 @@ class Animator:
         vmax = dynamic_range - actual_brightness
 
         return vmin, vmax
+
+    def highlight_current_area(self, x, y):
+        pixel_color = VISITED_COLORMAP(self.normalized_fft_brightness[y, x])[:3]
+        img_x = (x + self.size // 2) % self.size
+        img_y = (y + self.size // 2) % self.size
+        self.visited_fft_image[img_x, img_y] = pixel_color
+
 
 
 image = Image.open("input/stop.jpg")
@@ -95,13 +120,16 @@ fig, ax = plt.subplots(nrows=2, ncols=2)
 ax[0, 0].imshow(image_data, cmap="gray", vmin=0, vmax=255)
 ax[0, 0].set_title("Original Image")
 
-ax[0, 1].imshow(np.log10(np.abs(np.fft.fftshift(image_fft)) + 1), cmap="viridis")
-ax[0, 1].set_title("FFT")
-
-animator = Animator(ax[1, 0], ax[1, 1], image_fft)
-
+animator = Animator(
+    image_ax=ax[1, 0],
+    layer_ax=ax[1, 1],
+    fft_ax=ax[0, 1],
+    fft=image_fft
+)
 
 current_step = 0
+
+
 def draw_next_step():
     global current_step
 
